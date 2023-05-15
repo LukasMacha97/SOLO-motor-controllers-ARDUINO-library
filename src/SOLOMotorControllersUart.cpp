7ca1c1ec4c083a41139c83f564ee87f8f15c8a88
Lukas Macha <lm2292@bath.ac.uk>
2023-05-15 16:36:53 +0100

    adding Serial_

--- src/SOLOMotorControllersUart.cpp
+++ src/SOLOMotorControllersUart.cpp
@@ -75,7 +75,7 @@ bool SOLOMotorControllersUart::ExeCMD(unsigned char cmd[], int &error)
 
         //DELAY for default serial of Arduino 
         //(Arduino serial is shared with usb line and can be over noise line)
-        if(serialToUse == &Serial)
+        if(serialToUse == &Serial_)
         {
             //Serial.println("DELAY");
             delay(50);
